19604d150253e2d21ba9c14fa2d519a76f348695
Mateusz Cerulewicz <75334163+mateuszcer@users.noreply.github.com>
2023-06-17 15:33:21 +0200

    Add Service Implementation to Dependency (#6)

--- lib/src/main/java/framework/model/Dependency.java
+++ lib/src/main/java/framework/model/Dependency.java
@@ -1,21 +1,4 @@
 package framework.model;
 
-public class Dependency {
-    private final Class<?> client;
-
-    private final Class<?> dependsOn;
-
-    public Dependency(Class<?> client, Class<?> dependsOn) {
-        this.client = client;
-        this.dependsOn = dependsOn;
-    }
-
-
-    public Class<?> getClient() {
-        return client;
-    }
-
-    public Class<?> getDependsOn() {
-        return dependsOn;
-    }
+public record Dependency(Class<?> client, Class<?> service, Class<?> implementation) {
 }
